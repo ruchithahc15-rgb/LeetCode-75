f01eeef5b7eb7bdb03e90865151f644b23e8d66b
Ruchitha HC <ruchithahc15@gmail.com>
2026-08-01 22:04:10 +0530

    Time: 72 ms (31.71%), Space: 50.3 MB (80.41%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,18 +5,39 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        prev = None
-        slow, fast = head, head
+        # Step 1: Find the middle of the linked list
+        slow = head
+        fast = head
         while fast and fast.next:
-            fast = fast.next.next
-            temp = slow.next
-            slow.next = prev 
-            prev = slow
-            slow = temp
-        res = 0
-        while slow:
-            res = max(res,prev.val+slow.val)
-            prev = prev.next
             slow = slow.next
-        return res
+            fast = fast.next.next
+        
+        # 'slow' is now at the beginning of the second half
+        
+        # Step 2: Reverse the second half of the list
+        prev = None
+        curr = slow
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            
+        # 'prev' is now the head of the reversed second half
+        
+        # Step 3: Find the maximum twin sum
+        max_sum = 0
+        first_half = head
+        second_half = prev
+        
+        while second_half: # Only need to loop through half the list
+            current_twin_sum = first_half.val + second_half.val
+            max_sum = max(max_sum, current_twin_sum)
+            
+            # Move both pointers forward
+            first_half = first_half.next
+            second_half = second_half.next
+            
+        return max_sum
+
         
