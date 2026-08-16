13e18006025df5c0f4cf0d6b41a737a58bdbe9da
Ruchitha HC <ruchithahc15@gmail.com>
2026-08-16 22:31:37 +0530

    Time: 0 ms (100%), Space: 19.5 MB (39.19%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,12 +1,37 @@
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        quot = collections.defaultdict(dict)
-        for (num, den), val in zip(equations, values):
-            quot[num][num] = quot[den][den] = 1.0
-            quot[num][den] = val
-            quot[den][num] = 1 / val
-        for k, i, j in itertools.permutations(quot, 3):
-            if k in quot[i] and j in quot[k]:
-                quot[i][j] = quot[i][k] * quot[k][j]
-        return [quot[num].get(den, -1.0) for num, den in queries]
-        
+    def calcEquation(self, equations, values, queries):
+
+        graph = collections.defaultdict(list)
+
+        # Build graph
+        for (a, b), value in zip(equations, values):
+            graph[a].append((b, value))
+            graph[b].append((a, 1 / value))
+
+        def dfs(current, target, visited):
+
+            if current == target:
+                return 1.0
+
+            visited.add(current)
+
+            for neighbor, value in graph[current]:
+                if neighbor not in visited:
+
+                    result = dfs(neighbor, target, visited)
+
+                    if result != -1.0:
+                        return value * result
+
+            return -1.0
+
+        answer = []
+
+        for a, b in queries:
+
+            if a not in graph or b not in graph:
+                answer.append(-1.0)
+            else:
+                answer.append(dfs(a, b, set()))
+
+        return answer
