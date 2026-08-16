25cbe73023d448c98d5f3661620b75a574d8ec05
Ruchitha HC <ruchithahc15@gmail.com>
2026-08-16 11:02:29 +0530

    Time: 91 ms (97.64%), Space: 79.2 MB (42.02%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,23 +1,23 @@
 class Solution:
-    def dfs(self, i, visited, adj, ans):
-        visited[i] = True
-
-        for node, direction in adj[i]:
-            if not visited[node]:
-                if direction:
-                    ans[0] += 1
-
-                self.dfs(node, visited, adj, ans)
-
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        ans = [0]
-        visited = [False] * n
         adj = [[] for _ in range(n)]
 
-        for u, v in connections:
-            adj[u].append((v, True))
-            adj[v].append((u, False))
+        for a, b in connections:
+            adj[a].append((b, 1))
+            adj[b].append((a, 0))
 
-        self.dfs(0, visited, adj, ans)
+        ans = [0]
+        visited = [False] * n
 
+        def dfs(node):
+            visited[node] = True
+
+            for neighbor, direction in adj[node]:
+                if not visited[neighbor]:
+                    if direction == 1:
+                        ans[0] += 1
+
+                    dfs(neighbor)
+
+        dfs(0)
         return ans[0]
